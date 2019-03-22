c0785c369f979244ec581172d694dc0c3fb5db36
Peter Havens <peter.havens@gmail.com>
2019-03-22 11:17:34 -0600

    Returning from loops.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    loop {
-        println!("Hello, world!");
-    }
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
 }
